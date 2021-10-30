f329050a4c02438eb161e0fb905edc3008c7f940
PossiblePack <possible.pack@gmail.com>
2021-10-30 15:51:54 +0700

    Finished!!

--- Fuzzy.py
+++ Fuzzy.py
@@ -2,20 +2,21 @@ import math
 
 print('Implement Fuzzy logic')
 
+#input
 input_temperature     = input('Temperture     = ')
 input_humidity        = input('Humidity       = ')
 input_Soil_moisture   = input('Soil moisture  = ')
 input_Light_intesity  = input('Light intesity = ')
 
-
+#store input to variable
 temp     = int(input_temperature)
 humidity = int(input_humidity)
 soil     = int(input_Soil_moisture)
-light    = int(input_Light_intesity)
+light    = float(input_Light_intesity)
 
 #Implement Linguistic variable
 #Temperature
-if(temp == 0):
+if(temp <= 25):
     temp_cool = 1
     temp_warm = 0
     temp_hot = 0
@@ -37,7 +38,7 @@ else:
     temp_hot = 1
 
 #Humidity
-if(humidity == 0):
+if(humidity <= 25):
     humidity_dry = 1
     humidity_humid = 0
     humidity_damp = 0
@@ -59,7 +60,7 @@ else:
     humidity_damp = 1
 
 #soil moisture
-if(soil == 0):
+if(soil <= 25):
     soil_dry = 1
     soil_moist = 0
     soil_moistly = 0
@@ -81,81 +82,111 @@ else:
     soil_moistly = 1
 
 #Light intensity
-if(light == 0):
-    value_light = 0
+if(light <= 0.5):
+    value_bright = 0
     value_dark  = 1
-elif(light < 0.5):
-    value_light = (0.5 - light) / (0.5) 
-    value_dark  = (light-0.5) / (0.5)
-elif(light > 0.5 and light < 6.5):
-    value_light = 1
+elif(light <= 1):
+    value_bright = (1 - light) / (1) 
+    value_dark  = (light-1) / (1)
+elif(light > 1):
+    value_bright = 1
     value_dark  = 0
-else:
-    value_light = 0
-    value_dark  = 0
-
-# print('Temperation value is \n dry = {} \n moist = {} \n moistly = {}'.format(temp_dry,temp_moist,temp_moistly))
-# print('Humidity value is \n dry = {} \n humid = {} \n damp = {}'.format(humidity_dry,humidity_humid,humidity_damp))
-# print('Soil moisture value is \n dry = {} \n moist = {} \n moistly = {}'.format(soil_dry,soil_moist,soil_moistly))
-# print('Light intensity value is \n light = {} \n dark = {}'.format(value_light,value_dark))
 
 #for add result to water pump status
 waterpump_status = []
-def waterpump_on(temp, humidity, soil):
+def waterpump_on1(soil):
+    if soil != 0:
+        waterpump_status.append([soil, 1])
+
+def waterpump_on2(humidity, soil):
+    if humidity != 0 and soil != 0:
+        result = min(humidity,soil)
+        waterpump_status.append([result, 1])
+
+def waterpump_on3(temp, humidity, soil):
     if temp !=0 and humidity != 0 and soil != 0:
         result = min(temp,humidity,soil)
         waterpump_status.append([result, 1])
 
-def waterpump_off(temp, humidity, soil):
+def waterpump_off1(soil):
+    if soil != 0:
+        waterpump_status.append([soil, 0])
+
+def waterpump_off3(temp, humidity, soil):
     if temp !=0 and humidity != 0 and soil != 0:
         result = min(temp,humidity,soil)
         waterpump_status.append([result, 0])
 
 #calculate water pump status
-waterpump_on(2, 2, soil_dry) # value = 2 is dont' care that value
-waterpump_on(2, humidity_humid, soil_moist)
-waterpump_off(2, 2, soil_moistly)
-waterpump_off(temp_cool, humidity_damp, soil_moist)
+waterpump_on1(soil_dry)
+waterpump_on2(humidity_humid, soil_moist)
+waterpump_off1(soil_moistly)
+waterpump_off3(temp_cool, humidity_damp, soil_moist)
 
 #for add result to fan 1,2 status
 fan_N01_status =[]
 fan_N02_status =[]
-def fan_N01_on(temp, humidity):
+def fan_N01_on1(temp):
+    if temp !=0:
+        fan_N01_status.append([temp, 1])
+
+def fan_N01_off1(temp):
+    if temp !=0:
+        fan_N01_status.append([temp, 0])
+
+def fan_N01_on2(temp, humidity):
     if temp !=0 and humidity != 0:
         result = min(temp,humidity)
-        fan_N01_status.append([result, 1])
+        fan_N02_status.append([result, 0])
 
-def fan_N01_off(temp, humidity):
+def fan_N01_off2(temp, humidity):
     if temp !=0 and humidity != 0:
         result = min(temp,humidity)
-        fan_N01_status.append([result, 0])
+        fan_N02_status.append([result, 0])
 
-def fan_N02_on(temp, humidity):
+def fan_N02_on1(temp):
+    if temp !=0:
+        fan_N02_status.append([temp, 1])
+
+def fan_N02_on2(temp, humidity):
     if temp !=0 and humidity != 0:
         result = min(temp,humidity)
-        fan_N02_status.append([result, 1])
+        fan_N02_status.append([result, 0])
 
-def fan_N02_off(temp, humidity):
+def fan_N02_off1(temp):
+    if temp !=0:
+        fan_N02_status.append([temp, 0])
+
+def fan_N02_off2(temp, humidity):
     if temp !=0 and humidity != 0:
         result = min(temp,humidity)
-        fan_N02_status.append([result, 0 ])
+        fan_N02_status.append([result, 0])
 
-fan_N01_on(temp_warm, 2)
-fan_N02_on(temp_hot, 2)
-fan_N01_off(temp_cool, humidity_damp)
-fan_N02_off(temp_cool, humidity_humid)
+#calculate fan 1,2 status
+fan_N01_on1(temp_warm)
+fan_N02_off1(temp_warm)
+fan_N01_on1(temp_hot)
+fan_N02_on1(temp_hot)
+fan_N01_off1(temp_cool)
+fan_N02_off1(temp_cool)
+fan_N01_on2(temp_warm, humidity_damp)
+fan_N02_on2(temp_warm, humidity_damp)
+fan_N01_off2(temp_warm, humidity_humid)
+fan_N02_off2(temp_warm, humidity_humid)
 
+#for add result to light bulb status
 lightbulb_status = []
 def lightbulb_on(light):
     lightbulb_status.append([light, 1])
 
-def lightbulb_off(temp):
-    result = min(temp,humidity)
-    lightbulb_status.append([result, 0])
+def lightbulb_off(light):
+    lightbulb_status.append([light, 0])
 
+#calculate light bulb status
 lightbulb_on(value_dark)
-lightbulb_off(value_light)
+lightbulb_off(value_bright)
 
+#defuzzification for output
 def defuzzy(list):
     multiply_new = 0
     split_new = 0
@@ -165,17 +196,15 @@ def defuzzy(list):
         split = list[i][0]
         multiply_new += multiply
         split_new += split
+    if(split_new <= 0):
+        split_new = 1
     x = multiply_new / split_new
 
     return x
 
-if(temp <= 40 and humidity <= 100 and soil <= 100 and light <= 7):
-    
-    print('Water pump is ',math.ceil(defuzzy(waterpump_status)))
-    print('fan 1 is ',math.ceil(defuzzy(fan_N01_status)))
-    print('fan 2 is ',math.ceil(defuzzy(fan_N02_status)))
-    print('light bulb is ',math.ceil(defuzzy(fan_N01_status)))
-else:
-    print('your value is out of range')
-
+#print result of fuzzy control
+print('Value of water pump is ',math.ceil(defuzzy(waterpump_status)))
+print('Value of fan 1 is ',math.ceil(defuzzy(fan_N01_status)))
+print('Value of fan 2 is ',math.ceil(defuzzy(fan_N02_status)))
+print('Value of light bulb is ',math.ceil(defuzzy(lightbulb_status)))
 
